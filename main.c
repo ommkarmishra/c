18d06a4ba1e1dc872b7e2f327c941f2dead57c1b
Ommkar <ommkar2003@gmail.com>
2026-07-26 00:00:00 +0530

    Iniial commit

---
+++ main.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <string.h>
+#include<stdbool.h>
+int main(){
+
+ /*   int age = 0;
+    float gpa = 0.0f;
+    char n[20] = "";  //use for string 
+    char sing = '*';  //for cahracter
+
+    printf("Age: ");
+    scanf("%d", &age);
+
+    printf("GPA: ");
+    scanf("%f", &gpa);
+
+    getchar();
+    printf("Name:");
+    fgets(n, sizeof(n), stdin);
+  
+
+     
+    printf("%d\n", age);
+    printf("%.3f\n", gpa);
+    printf("%c%s",sing, n);
+   */
+
+   /* char item[50] = "";
+    float price = 0.0f;
+    int quantity = 0;
+    char currency = '$';
+    float total = 0.0f;
+
+    printf("What item would you like to buy?: ");
+    fgets(item, sizeof(item), stdin);
+    item[strlen(item)-1] = '\0';
+
+    printf("What is the price for each?: ");
+    scanf("%f", &price);
+
+    printf("How many would you like?: ");
+    scanf("%d", &quantity);
+
+    total = price * quantity;
+
+    printf("\nYou brought %d %s at ", quantity, item);
+    printf("%c%.2f", currency, total);
+    */
+
+    /*char noun[50] = "";
+    char verb[50] = "";
+    char adjective1[50] = "";
+    char adjective2[50] = "";
+    char adjective3[50] = "";
+
+    printf("Enter an adjective (description): ");
+    fgets(adjective1, sizeof(adjective1), stdin);
+    adjective1[strlen(adjective1)-1] = '\0';
+
+    printf("Enter a noun (animal or person): ");
+    fgets(noun, sizeof(noun), stdin);
+    noun[strlen(noun)-1] = '\0';
+
+    printf("Enter an adjective (description): ");
+    fgets(adjective2, sizeof(adjective2), stdin);
+    adjective2[strlen(adjective2)-1] = '\0';
+
+    printf("Enter a verb (ending w/ -ing): ");
+    fgets(verb, sizeof(verb), stdin);
+    verb[strlen(verb)-1] = '\0';
+
+
+    printf("%s", adjective1);
+    printf("%s ", noun);
+    printf("%s ", adjective2);
+    printf("%s.\n", verb);
+    */
+
+   
+
+
+
+
+    return 0;
+}
