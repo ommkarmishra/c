8b1059c075a7c16fbc180db1103ed508c6be86b7
Ommkar <ommkar2003@gmail.com>
2026-07-27 01:39:28 +0530

    added

---
+++ learning/l1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include<stdbool.h>
+
+int main(){
+
+    int n = 0;
+
+    while (n <= 0)
+    {
+        printf("num: ");
+        scanf("%d", &n);
+    }
+    
+    
+
+    return 0;
+
+}
