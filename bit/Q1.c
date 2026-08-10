e01f43afd16abc370afeb2e9cb34b91099cf3111
Ommkar <ommkar2003@gmail.com>
2026-08-10 12:06:44 +0530

    bit1

---
+++ bit/Q1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include<stdbool.h>
+
+/*Write a function setbits(x,p,n,y) that returns x with the n bits that begin at 
+position p set to the rightmost n bits of y, leaving the other bits unchanged.*/
+
+int setbits(x,p,n,y){
+
+
+
+    return n;
+}
+
+int main(){
+
+    // x = 10 110 110
+    //y = 00000 101
+    int p = 5;
+    int n= 3;
+    int x = 0;
+
+    return 0;
+}
