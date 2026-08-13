9c7fd12d5cc687c7cde0b07edbdef4b6914c4a65
Ommkar <ommkar2003@gmail.com>
2026-08-13 13:00:17 +0530

    integer n into a base b character representation in the string s

---
+++ learning/l2.c
@@ -0,0 +1,2 @@
+/*Exercise 2-5. Write the function any(s1,s2), which returns the first location in a string s1
+where any character from the string s2 occurs, or -1 if s1 contains no characters from s2.*/
---
+++ learning/l3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include<stdbool.h>
+
+
+int main(){
+/*int i, n  = 10;
+    int a[] = {10,20,30,40,50,60,70,80,90,100,110,120};
+   for (i = 0; i < n; i++){
+    printf("%6d%c", a[i], (i%10==9 || i==n-1) ? '\n' : ' ');
+   }*/
+
+   /*
+   /*upper to lower
+int lower(int c){
+    return (c >= 'A' && c <= 'Z' ? c + 'a' - 'A' : c);
+}
+   char c = 'D';
+   printf("%c\n", lower(c));
+   */
+  
+
+
+    return 0;
+}
