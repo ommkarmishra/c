cebdcc450a4e9f6a81a437f6e7bd25ea82968940
Ommkar <ommkar2003@gmail.com>
2026-08-16 21:18:03 +0530

    4.1 added

--- learning/l3.c
+++ learning/l3.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 #include <string.h>
 #include<stdbool.h>
+/*Write the function itob(n,s,b) that converts the integer n into a base b character representation in the string s.
+In particular, itob(n,s,16) formats s as a hexadecimal integer in s.
+void itob(int n,char s[]){
+}*/
+/*4-1. Write the function strindex(s,t) which returns the position of the rightmost
+occurrence of t in s, or -1 if there is none*/
+int strindex(char s[], char t[]){
 
+    int i , j, k;
+    for(i = strlen(s) - strlen(t); i >= 0; i--){
+        for(j = i, k= 0; t[k] != '\0' && s[j] == t[k]; j++, k++){
+            ;
+        }
+        if(t[k] == '\0'){
+            return i;  
+        }
+    }
 
+    return -1;
+}
 int main(){
 /*int i, n  = 10;
     int a[] = {10,20,30,40,50,60,70,80,90,100,110,120};
@@ -18,8 +36,10 @@ int lower(int c){
    char c = 'D';
    printf("%c\n", lower(c));
    */
-  
+  char s[100] = "the quick brown fox jumps over the quick fox";
+  char t[100] = "cat";
 
+  printf("%d\n", strindex(s,t));
 
     return 0;
 }
