d359426787a7e44d56d1e119936b3e82b2148f87
Ommkar <ommkar2003@gmail.com>
2026-08-20 00:00:13 +0530

    pointers start

--- learning/l2.c
+++ learning/l2.c
@@ -1,2 +1,7 @@
 /*Exercise 2-5. Write the function any(s1,s2), which returns the first location in a string s1
 where any character from the string s2 occurs, or -1 if s1 contains no characters from s2.*/
+
+/*Exercise 4-12. Adapt the ideas of printd to write a recursive version of itoa; that is, convert
+an integer into a string by calling a recursive routine.
+Exercise 4-13. Write a recursive version of the function reverse(s), which reverses the
+string s in place.*/
