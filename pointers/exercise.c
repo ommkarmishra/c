bf2864dc7293ef67efe3658eb81eaad789a44819
Ommkar <ommkar2003@gmail.com>
2026-08-22 22:00:14 +0530

    ex5.1

---
+++ pointers/exercise.c
@@ -0,0 +1,15 @@
+/*Exercise 5-1: In an integr if there is a sign at first, then put the sign at the end eg.;-123 -> 123-
+*/
+#include <stdio.h>
+#include <string.h>
+#include<ctype.h>
+int getint(int *ptr){
+
+}
+int main(){
+
+
+   
+
+    return 0;
+}
